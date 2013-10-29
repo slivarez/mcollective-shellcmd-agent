ca07ba81c33b571a5adb1531b5511668ce7bad64
root <root@puppet-y.server-pool.net>
2013-10-29 15:12:51 -0400

    Exception handling has been added to the agent. From now any failed command will be correctly reported instead of crashing "mco" run.

--- agent/shellcmd.rb
+++ agent/shellcmd.rb
@@ -10,14 +10,18 @@ module MCollective
       reply[:stdout] = ""
       reply[:stderr] = ""
 
-      exitcode = Open4.popen4("#{request[:cmd]}") do |pid, stdin, stdout, stderr|
-         # Just in case we execute a program that waits for stdin by accident
-         stdin.reopen(File::open('/dev/null', 'r'))
-
-         reply[:stdout] << stdout.gets until stdout.eof?
-         reply[:stderr] << stderr.gets until stderr.eof?
+      begin
+         exitcode = Open4.popen4("#{request[:cmd]}") do |pid, stdin, stdout, stderr|
+            # Just in case we execute a program that waits for stdin by accident
+            stdin.reopen(File::open('/dev/null', 'r'))
+   
+            reply[:stdout] << stdout.gets until stdout.eof?
+            reply[:stderr] << stderr.gets until stderr.eof?
+         end
+      rescue Exception => e
+         reply[:stderr] << e.to_s
+         exitcode = 1
       end
-
       reply[:exitcode] = exitcode
     end
   end
